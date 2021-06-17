a44bd9f1e26117a18cbea89fb6ff0aa96404730b
Ayush Mayank <ayushmayank20@gmail.com>
2021-06-17 08:08:00 +0530

    Add existing file

--- navigation/app.navigation.js
+++ navigation/app.navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import 'react-native-gesture-handler';
 import WelcomeScreen from "../screens/WelcomeScreen";
 import LoginScreen from "../screens/LoginScreen";
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ gestureEnabled: false, cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,}}>
         <Stack.Screen options={{headerShown: false}} name="Welcome" component={WelcomeScreen} />
         <Stack.Screen  name="Login to continue" component={LoginScreen} />
       </Stack.Navigator>
